6d715ad0154edc1733d43e35572f0a6bb950506d
Rafael Fonseca <rfonseca985@gmail.com>
2023-07-19 16:12:41 -0300

    Criado estados e cidades

--- src/main/java/com/rfonseca985/loja/LojaBackendApplication.java
+++ src/main/java/com/rfonseca985/loja/LojaBackendApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rfonseca985.loja.domain.Categoria;
+import com.rfonseca985.loja.domain.Cidade;
+import com.rfonseca985.loja.domain.Estado;
 import com.rfonseca985.loja.domain.Produto;
 import com.rfonseca985.loja.repositories.CategoriaRepository;
+import com.rfonseca985.loja.repositories.CidadeRepository;
+import com.rfonseca985.loja.repositories.EstadoRepository;
 import com.rfonseca985.loja.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class LojaBackendApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaBackendApplication.class, args);
@@ -41,8 +49,20 @@ public class LojaBackendApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 	}
 
---
+++ src/main/java/com/rfonseca985/loja/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.rfonseca985.loja.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rfonseca985.loja.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+
+}
