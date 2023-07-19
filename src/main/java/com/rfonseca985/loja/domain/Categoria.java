6cdf9a485b1e87525b59c38b6ff0d4588f79917b
Rafael Fonseca <rfonseca985@gmail.com>
2023-07-19 13:17:13 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/rfonseca985/loja/domain/Categoria.java
+++ src/main/java/com/rfonseca985/loja/domain/Categoria.java
@@ -3,14 +3,24 @@ package com.rfonseca985.loja.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria implements Serializable{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+@Entity
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nome;
-	
+
 	public Categoria() {
-		
+
 	}
 
 	public Categoria(int id, String nome) {
@@ -51,6 +61,5 @@ public class Categoria implements Serializable{
 		Categoria other = (Categoria) obj;
 		return id == other.id;
 	}
-	
-	
+
 }
