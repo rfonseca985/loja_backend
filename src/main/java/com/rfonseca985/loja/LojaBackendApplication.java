0bfcb8c2a095e024ac7925eee407abcb43cfd4ae
Rafael Fonseca <rfonseca985@gmail.com>
2023-07-19 12:36:11 -0300

    Projeto criado, criadas classe de dominio, resource e REST testado

---
+++ src/main/java/com/rfonseca985/loja/LojaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rfonseca985.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/rfonseca985/loja/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.rfonseca985.loja.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rfonseca985.loja.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public List<Categoria> listar(){
+	Categoria cat1 = new Categoria(1, "Informática");
+	Categoria cat2 = new Categoria(2, "Escritório");
+	
+	List<Categoria> lista = new ArrayList<>();
+	lista.add(cat1);
+	lista.add(cat2);
+	
+	return lista;
+	}
+}
