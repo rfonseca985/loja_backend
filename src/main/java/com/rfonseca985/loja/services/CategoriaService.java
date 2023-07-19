b0814c0cc749e697f0ae8a0e1156617dff24473c
Rafael Fonseca <rfonseca985@gmail.com>
2023-07-19 15:47:22 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/rfonseca985/loja/services/CategoriaService.java
+++ src/main/java/com/rfonseca985/loja/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.rfonseca985.loja.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,8 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj  = repo.findById(id);
-		return obj.orElse(null);
-	}
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), obj));
+		} 
+
 }
