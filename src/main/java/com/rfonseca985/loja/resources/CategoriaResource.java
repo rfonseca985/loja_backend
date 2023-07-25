c1401769fef273344f8fded0e186603a1e688583
Rafael Fonseca <rfonseca985@gmail.com>
2023-07-25 15:40:24 -0300

    Criado Post de Categoria

--- src/main/java/com/rfonseca985/loja/resources/CategoriaResource.java
+++ src/main/java/com/rfonseca985/loja/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.rfonseca985.loja.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rfonseca985.loja.domain.Categoria;
 import com.rfonseca985.loja.services.CategoriaService;
@@ -21,8 +26,12 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id){
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok(obj);
-		
-		
-
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/(id)").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
